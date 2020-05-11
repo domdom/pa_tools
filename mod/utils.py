18dfb81e9138bcdccaef63f41702aaf5825e4d49
domdom <domdom314@gmail.com>
2020-05-11 20:13:42 +1000

    Fix bugs keeping track of files added to pa directory in debug mode

--- mod/utils.py
+++ mod/utils.py
@@ -7,13 +7,19 @@ from pa_tools.pa import paths
 from pa_tools.pa import pajson
 
 
-def create_pafs(is_titans=True):
-    src = pafs(paths.PA_MEDIA_DIR)
+def create_pafs(is_titans=True, mount_backup=True):
+    backup_path = _join(paths.PA_MEDIA_DIR, 'backup')
 
-    src.mount('/pa/', paths.PA_MEDIA_DIR + '/backup/pa')
+    mount_backup = mount_backup and os.path.exists(backup_path)
+
+    src = pafs(paths.PA_MEDIA_DIR)
+    if mount_backup:
+        src.mount('/pa/', _join(backup_path, 'pa'))
+
     if is_titans:
         src.mount('/pa/', '/pa_ex1')
-        src.mount('/pa/', paths.PA_MEDIA_DIR + '/backup/pa_ex1')
+        if mount_backup:
+            src.mount('/pa/', _join(backup_path, '/pa_ex1'))
 
     return src
 
@@ -27,18 +33,20 @@ def _copy(src, dst):
     os.makedirs(os.path.dirname(dst), exist_ok=True)
     shutil.copyfile(src, dst)
 
-def deploy_debug(mod_dir):
+def deploy_debug(mod_dir, is_titans):
+    print('======== DEBUG_DEPLOY')
     backup_path = paths.PA_MEDIA_DIR + '/backup'
     print('Creating backup directory', backup_path)
     os.makedirs(backup_path, exist_ok=True)
 
-    src = pafs(paths.PA_MEDIA_DIR)
-    src.mount('/pa/', '/pa_ex1')
-
+    src = create_pafs(is_titans, False)
 
     backup_log_file = backup_path + '/log.json'
 
     log = set(pajson.loadf(backup_log_file)[0] if os.path.isfile(backup_log_file) else [])
+
+    create_list = []
+    replace_list = []
 
     for root, dirs, files in os.walk(mod_dir):
         for file in files:
@@ -53,35 +61,53 @@ def deploy_debug(mod_dir):
             pa_file = src.resolveFile(path)
 
 
-            if pa_file == None:
-                log.add(path)
-
-                # copy file
-                _copy(mod_file, _join(paths.PA_MEDIA_DIR, path))
-
+            if pa_file == None or path in log:
+                create_list.append((mod_file, path))
             else:
-                pa_path = pa_file.replace(paths.PA_MEDIA_DIR, '')
+                replace_list.append((mod_file, pa_file))
 
-                bfile = _join(backup_path, pa_path)
+    for mod_file,path in create_list:
+        if is_titans and path.startswith('/pa/'):
+            path = '/pa_ex1/' + path[len('/pa/'):]
 
-                if not os.path.isfile(bfile):
-                    # do backup
-                    _copy(pa_file, bfile)
+        print ('creating file:', path)
+        log.add(path)
 
-                # copy file
-                _copy(mod_file, pa_file)
+        # copy file
+        _copy(mod_file, _join(paths.PA_MEDIA_DIR, path))
+
+    for mod_file,pa_file in replace_list:
+        print ('copy:', mod_file, pa_file)
+        pa_path = pa_file.replace(paths.PA_MEDIA_DIR, '')
+
+        bfile = _join(backup_path, pa_path)
+
+        if not os.path.isfile(bfile):
+            # do backup
+            _copy(pa_file, bfile)
+
+        # copy file
+        _copy(mod_file, pa_file)
 
 
     with open(backup_log_file, 'w') as f:
         pajson.dump(list(log), f, indent=2)
 
 def restore():
+    print('======== Restore')
     backup_path = paths.PA_MEDIA_DIR + '/backup'
     backup_log_file = backup_path + '/log.json'
     log = pajson.loadf(backup_log_file)[0] if os.path.isfile(backup_log_file) else []
 
     fs = pafs(paths.PA_MEDIA_DIR)
     fs.mount('/pa/', '/pa_ex1')
+
+    for file in log:
+        with contextlib.suppress(FileNotFoundError):
+            to_remove = _join(paths.PA_MEDIA_DIR, file);
+            print('removed:', to_remove)
+            os.remove(to_remove)
+
 
     for root, dirs, files in os.walk(backup_path):
         for file in files:
@@ -95,9 +121,7 @@ def restore():
 
             _copy(src, dst)
 
-    for file in log:
-        with contextlib.suppress(FileNotFoundError):
-            os.remove(_join(paths.PA_MEDIA_DIR, file))
+            print('restoring:', dst)
 
     # remove backup
     shutil.rmtree(backup_path, ignore_errors=True)
